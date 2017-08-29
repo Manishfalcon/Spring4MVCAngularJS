d2aa78232e4ea1ab697ab8f6fee58e9be860a929
manishfalcon <manishmca16@gmail.com>
2017-08-29 18:21:02 +0530

    First commit Spring4 MVC with Angular

---
+++ src/main/java/com/hm/springmvc/model/User.java
@@ -0,0 +1,123 @@
+package com.hm.springmvc.model;
+
+/**
+ * 
+ * @author Manish.sharma
+ *
+ */
+public class User {
+	private long id;
+
+	private String username;
+
+	private String address;
+
+	private String email;
+
+	private String subject;
+
+	public User() {
+		id = 0;
+	}
+
+	public User(long id, String username, String address, String email, String subject) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.address = address;
+		this.email = email;
+		this.subject = subject;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", address=" + address + ", email=" + email + ", subject="
+				+ subject + "]";
+	}
+}
